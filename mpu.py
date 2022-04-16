820be6b4ea501f7a7136da1de0923dbe7272645a
dam-di <cfgs.ddi@gmail.com>
2022-04-16 23:41:39 +0200

    Actualizar

--- mpu.py
+++ mpu.py
@@ -5,20 +5,26 @@ import sys
 mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 
 try:
+    z = 0
+    y = 0
+    x = 0
+
     while True:
-        accel = mpu9250.readAccel()
-        print (") ax = " , ( accel['x'] ))
-        print (" ay = " , ( accel['y'] ))
-        print (" az = " , ( accel['z'] ))
+        # accel = mpu9250.readAccel()
+        # print (") ax = " , ( accel['x'] ))
+        # print (" ay = " , ( accel['y'] ))
+        # print (" az = " , ( accel['z'] ))
         gyro = mpu9250.readGyro()
         print (" gx = " , ( gyro['x'] ))
         print (" gy = " , ( gyro['y'] ))
         print (" gz = " , ( gyro['z'] ))
-
-        mag = mpu9250.readMagnet()
-        print (" mx = " , ( mag['x'] ))
-        print (" my = " , ( mag['y'] ))
-        print (" mz = " , ( mag['z'] ))
+        z = z + gyro['z']
+        y = y + gyro['y']
+        x = x + gyro['x']
+        # mag = mpu9250.readMagnet()
+        # print (" mx = " , ( mag['x'] ))
+        # print (" my = " , ( mag['y'] ))
+        # print (" mz = " , ( mag['z'] ))
 
 
         time.sleep(0.1)
