f45bcfa8873e69156bd11bb0bb8c5f822d9a4bc5
dam-di <cfgs.ddi@gmail.com>
2022-04-17 01:35:24 +0200

    Actualizar

--- cube.py
+++ cube.py
@@ -26,7 +26,7 @@ class My3D(App):
         self.renderer.camera.aspect = aspect
 
     def rotate_cube(self, anglex):
-        self.cube.rotation.x = numpy.np.rad2deg(anglex)
+        self.cube.rotation.x = (anglex * 180) / 3.1416
 
     def build(self):
         layout = FloatLayout()
