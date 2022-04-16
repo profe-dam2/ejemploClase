b93081c69b104a641b2272eff4e31d94d35a36c3
dam-di <cfgs.ddi@gmail.com>
2022-04-17 01:51:38 +0200

    Actualizar

--- cube.py
+++ cube.py
@@ -30,16 +30,23 @@ GYRO_YOUT = 0x45
 GYRO_ZOUT = 0x47
 PWR_MGMT_1 = 0x6b  # PWR_MGMT_1
 PWR_MGMT_2 = 0x6c  # PWR_MGMT_2
-zGyroAngleValue = 0
-yAngleValue = 0
-xAngleValue = 0
-xAxisAngleValue = 0
-yAxisAngleValue = 0
+
 bus = smbus.SMBus(1)
 
 bus.write_byte_data(DEV_ADDR, PWR_MGMT_1, 0)
 
 class My3D(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.xAxisValue = 0
+        self.zAxisValue = 0
+        self.yAxisValue = 0
+        self.zGyroAngleValue = 0
+        self.yAngleValue = 0
+        self.xAngleValue = 0
+        self.xAxisAngleValue = 0
+        self.yAxisAngleValue = 0
+
     def _adjust_aspect(self, *args):
         rsize = self.renderer.size
         aspect = rsize[0] / float(rsize[1])
@@ -51,6 +58,8 @@ class My3D(App):
         self.cube.rotation.x = (anglex * 180) / 3.1416
 
     def build(self):
+
+
         layout = FloatLayout()
 
         # create renderer
@@ -89,59 +98,97 @@ class My3D(App):
 
         return layout
 
-
-
-#s = serial.Serial('/dev/rfcomm0', 9600)
-
-
-# 2byte read
-def read_word(adr):
-    high = bus.read_byte_data(DEV_ADDR, adr)
-    low = bus.read_byte_data(DEV_ADDR, adr + 1)
-    val = (high << 8) + low
-    return val
-
-
-# Sensor data read
-def read_word_sensor(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):  # minus
-        return -((65535 - val) + 1)
-    else:  # plus
+    # 2byte read
+    def read_word(adr):
+        high = bus.read_byte_data(DEV_ADDR, adr)
+        low = bus.read_byte_data(DEV_ADDR, adr + 1)
+        val = (high << 8) + low
         return val
 
 
-# temperture
-def get_temp():
-    temp = read_word_sensor(TEMP_OUT)
-    x = temp / 340 + 36.53
-    return x
+    # Sensor data read
+    def read_word_sensor(self, adr):
+        val = self.read_word(adr)
+        if (val >= 0x8000):  # minus
+            return -((65535 - val) + 1)
+        else:  # plus
+            return val
 
 
-# gyro data
-# full scale range ±250 deg/s LSB sensitivity 131 LSB/deg/s -> ±250 x 131 = ±32750 LSB
-def get_gyro_data_deg():
-    x = read_word_sensor(GYRO_XOUT)
-    y = read_word_sensor(GYRO_YOUT)
-    z = read_word_sensor(GYRO_ZOUT)
-
-    x = x / 131.0
-    y = y / 131.0
-    z = z / 131.0
-    return [x, y, z]
+    # temperture
+    def get_temp(self):
+        temp = self.read_word_sensor(TEMP_OUT)
+        x = temp / 340 + 36.53
+        return x
 
 
-# accel data
-# full scale range ±2g LSB sensitivity 16384 LSB/g) -> ±2 x 16384 = ±32768 LSB
-def get_accel_data_g():
-    x = read_word_sensor(ACCEL_XOUT)
-    y = read_word_sensor(ACCEL_YOUT)
-    z = read_word_sensor(ACCEL_ZOUT)
+    # gyro data
+    # full scale range ±250 deg/s LSB sensitivity 131 LSB/deg/s -> ±250 x 131 = ±32750 LSB
+    def get_gyro_data_deg(self):
+        x = self.read_word_sensor(GYRO_XOUT)
+        y = self.read_word_sensor(GYRO_YOUT)
+        z = self.read_word_sensor(GYRO_ZOUT)
 
-    x = x / 16384.0
-    y = y / 16384.0
-    z = z / 16384.0
-    return [x, y, z]
+        x = x / 131.0
+        y = y / 131.0
+        z = z / 131.0
+        return [x, y, z]
+
+
+    # accel data
+    # full scale range ±2g LSB sensitivity 16384 LSB/g) -> ±2 x 16384 = ±32768 LSB
+    def get_accel_data_g(self):
+        x = self.read_word_sensor(ACCEL_XOUT)
+        y = self.read_word_sensor(ACCEL_YOUT)
+        z = self.read_word_sensor(ACCEL_ZOUT)
+
+        x = x / 16384.0
+        y = y / 16384.0
+        z = z / 16384.0
+        return [x, y, z]
+
+    def myBucle(self):
+        temp = self.get_temp()  # temperture
+        gyro_x, gyro_y, gyro_z = self.get_gyro_data_deg()  # gyro
+        accel_x, accel_y, accel_z = self.get_accel_data_g()  # accel
+
+        xGyroValue = float(gyro_x * 100) / 100 * 3.142 / 180
+        yGyroValue = float(gyro_y * 100) / 100 * 3.142 / 180
+        zGyroValue = float(gyro_z * 100) / 100 * 3.142 / 180
+        xAxisValue = float(accel_x * 100) / 100
+        yAxisValue = float(accel_y * 100) / 100
+        zAxisValue = float(accel_z * 100) / 100
+
+        # print(xGyroValue)
+        # print(yGyroValue)
+        # print(zGyroValue)
+        # print(xAxisValue)
+        # print(yAxisValue)
+        # print(zAxisValue)
+
+        self.zGyroAngleValue = self.zGyroAngleValue + zGyroValue * 0.05
+
+        try:
+            self.xAxisAngleValue = asin(min(0, max(self.yAxisValue / self.zAxisValue, -1)))
+            self.yAxisAngleValue = asin(min(0, max(self.xAxisValue / self.zAxisValue, -1)))
+        except:
+            print("ERROR")
+        self.xAngleValue = 0.98 * (
+                self.xAngleValue + xGyroValue * 0.05) + 0.02 * self.xAxisAngleValue
+        yAngleValue = 0.98 * (
+                self.yAngleValue - yGyroValue * 0.05) + 0.02 * self.yAxisAngleValue
+
+        print(self.xAngleValue)
+        print(self.yAngleValue)
+        print(self.zGyroAngleValue)
+
+        # rotateX(xAngleValue)
+        # rotateY(zGyroAngleValue)
+        # rotateZ(yAngleValue)
+
+        my3d.rotate_cube(self.xAngleValue)
+
+        time.sleep(0.05)
 
 
 my3d = My3D()
@@ -149,52 +196,7 @@ my3d.run()
 
 
 
-while 1:
-    temp = get_temp()  # temperture
-    gyro_x, gyro_y, gyro_z = get_gyro_data_deg()  # gyro
-    accel_x, accel_y, accel_z = get_accel_data_g()  # accel
 
 
-
-    xGyroValue = float(gyro_x * 100) / 100 * 3.142 / 180
-    yGyroValue = float(gyro_y * 100) / 100 * 3.142 / 180
-    zGyroValue = float(gyro_z * 100) / 100 * 3.142 / 180
-    xAxisValue = float(accel_x * 100) / 100
-    yAxisValue = float(accel_y * 100) / 100
-    zAxisValue = float(accel_z * 100) / 100
-
-    # print(xGyroValue)
-    # print(yGyroValue)
-    # print(zGyroValue)
-    # print(xAxisValue)
-    # print(yAxisValue)
-    # print(zAxisValue)
-
-
-    zGyroAngleValue = zGyroAngleValue + zGyroValue * 0.05
-
-
-    try:
-        xAxisAngleValue = asin(min(0,max(yAxisValue / zAxisValue,-1)))
-        yAxisAngleValue = asin(min(0,max(xAxisValue / zAxisValue,-1)))
-    except:
-        print("ERROR")
-    xAngleValue = 0.98 * (
-                xAngleValue + xGyroValue * 0.05) + 0.02 * xAxisAngleValue
-    yAngleValue = 0.98 * (
-                yAngleValue - yGyroValue * 0.05) + 0.02 * yAxisAngleValue
-
-    print(xAngleValue)
-    print(yAngleValue)
-    print(zGyroAngleValue)
-
-    # rotateX(xAngleValue)
-    # rotateY(zGyroAngleValue)
-    # rotateZ(yAngleValue)
-
-    my3d.rotate_cube(xAngleValue)
-
-
-    time.sleep(0.05)
 
 s.close()
